8104eb3e6415945b7a6dd23c92b9e936ae8e15c9
Stuart Smith <stuart78@gmail.com>
2014-07-20 09:57:10 -0700

    Index updates

---
+++ content/pages/portfolio.md
@@ -0,0 +1,6 @@
+Title: Portfolio
+Date: 2014-07-19
+
+
+# Portfolio
+This is some work I've done.  
--- pelicanconf.py
+++ pelicanconf.py
@@ -29,5 +29,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = 'themes/sfsp'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
